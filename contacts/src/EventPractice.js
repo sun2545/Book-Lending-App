0b8c2509813e183bba6776ea7a936811e8bd87b5
sun2545 <kwonsunhye0206@gmail.com>
2018-10-08 02:00:14 -0400

    practice of Event

---
+++ contacts/src/EventPractice.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class EventPractice extends Component {
+    render() {
+        return (
+            <div>
+                <h1>event practice</h1>
+                </div>
+        );
+    }
+}
+        
+    
